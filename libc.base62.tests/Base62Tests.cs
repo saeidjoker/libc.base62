15f14e854a71f4f2687a24e129ff82c95395f9af
Saeid Farahi <saeidfarahi@hotmail.com>
2020-02-02 13:12:29 +0330

    1.0.0 published

---
+++ libc.base62.tests/Base62Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+namespace libc.base62.tests {
+    public class Base62Tests {
+        [Fact]
+        public void Ascii() {
+            var ascii = "Hello I'm ASCII";
+            var base62 = Base62.Encode62(ascii);
+            var ascii_back = Base62.Decode62(base62);
+            Assert.Equal(ascii, ascii_back, StringComparer.Ordinal);
+        }
+        [Fact]
+        public void Unicode() {
+            var ascii = "سلام من اسکی هستم";
+            var base62 = Base62.Encode62(ascii);
+            var ascii_back = Base62.Decode62(base62);
+            Assert.Equal(ascii, ascii_back, StringComparer.Ordinal);
+        }
+    }
+}
---
+++ libc.base62/Base62.cs
@@ -0,0 +1,105 @@
+﻿using System.IO;
+using System.Text;
+namespace libc.base62 {
+    public static class Base62 {
+        private const string Base62CodingSpace = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        /// <summary>
+        ///     Convert a byte array
+        /// </summary>
+        /// <param name="original">Byte array</param>
+        /// <returns>Base62 string</returns>
+        public static string ToBase62(byte[] original) {
+            var sb = new StringBuilder();
+            var stream = new BitStream(original); // Set up the BitStream
+            var read = new byte[1]; // Only read 6-bit at a time
+            while (true) {
+                read[0] = 0;
+                var length = stream.Read(read, 0, 6); // Try to read 6 bits
+                if (length == 6) // Not reaching the end
+                {
+                    if (read[0] >> 3 == 0x1f) // First 5-bit is 11111
+                    {
+                        sb.Append(Base62CodingSpace[61]);
+                        stream.Seek(-1,
+                            SeekOrigin
+                                .Current); // Leave the 6th bit to next group
+                    } else if (read[0] >> 3 == 0x1e) // First 5-bit is 11110
+                    {
+                        sb.Append(Base62CodingSpace[60]);
+                        stream.Seek(-1, SeekOrigin.Current);
+                    } else // Encode 6-bit
+                    {
+                        sb.Append(Base62CodingSpace[read[0] >> 2]);
+                    }
+                } else if (length == 0) // Reached the end completely
+                {
+                    break;
+                } else // Reached the end with some bits left
+                {
+                    // Padding 0s to make the last bits to 6 bit
+                    sb.Append(Base62CodingSpace[read[0] >> (8 - length)]);
+                    break;
+                }
+            }
+            return sb.ToString();
+        }
+        /// <summary>
+        ///     Convert a Base62 string to byte array
+        /// </summary>
+        /// <param name="base62">Base62 string</param>
+        /// <returns>Byte array</returns>
+        public static byte[] FromBase62(string base62) {
+            // Character count
+            var count = 0;
+
+            // Set up the BitStream
+            var stream = new BitStream(base62.Length * 6 / 8);
+            foreach (var c in base62) {
+                // Look up coding table
+                var index = Base62CodingSpace.IndexOf(c);
+
+                // If end is reached
+                if (count == base62.Length - 1) {
+                    // Check if the ending is good
+                    var mod = (int) (stream.Position % 8);
+                    if (mod == 0)
+                        throw new InvalidDataException(
+                            "an extra character was found");
+                    if (index >> (8 - mod) > 0)
+                        throw new InvalidDataException(
+                            "invalid ending character was found");
+                    stream.Write(new[] {
+                            (byte) (index << mod)
+                        }, 0, 8 - mod);
+                } else {
+                    // If 60 or 61 then only write 5 bits to the stream, otherwise 6 bits.
+                    if (index == 60)
+                        stream.Write(new byte[] {
+                            0xf0
+                        }, 0, 5);
+                    else if (index == 61)
+                        stream.Write(new byte[] {
+                            0xf8
+                        }, 0, 5);
+                    else
+                        stream.Write(new[] {
+                            (byte) index
+                        }, 2, 6);
+                }
+                count++;
+            }
+
+            // Dump out the bytes
+            var result = new byte[stream.Position / 8];
+            stream.Seek(0, SeekOrigin.Begin);
+            stream.Read(result, 0, result.Length * 8);
+            return result;
+        }
+        public static string Encode62(string s) {
+            return ToBase62(Encoding.UTF8.GetBytes(s));
+        }
+        public static string Decode62(string s) {
+            return Encoding.UTF8.GetString(FromBase62(s));
+        }
+    }
+}
